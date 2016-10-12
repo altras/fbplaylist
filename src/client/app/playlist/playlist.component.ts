b8230d6d4af6da3dc38aabc0a7c128c32ac17c8a
Vitaliy Filipov <altras@gmail.com>
2016-10-12 22:30:54 +0300

    WIP integrate FB sdk

--- src/client/app/playlist/playlist.component.ts
+++ src/client/app/playlist/playlist.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+declare var FB: any;
+
 /**
  * This class represents the lazy loaded PlaylistComponent.
  */
@@ -18,6 +20,27 @@ export class PlaylistComponent implements OnInit {
   ngOnInit() {
     let queryParams: any = this.route.queryParams;
     this.link = queryParams.getValue().link
+    FB.init({
+      appId: '1673818472934251',
+      xfbml: true,
+      cookie: true,
+      version: 'v2.7'
+    })
+    FB.login(function(response: any) {
+      if (response.authResponse) {
+       console.log('Welcome!  Fetching your information.... ');
+       FB.api('/me', function(res: any) {
+         console.log('Good to see you, ' + res.name + '.');
+       });
+      } else {
+       console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+    // FB.api('/1741337529467978/feed', 'GET', {"fields":"link"},
+    //   function(res: any) {
+    //     console.log(res)
+    //   }
+    // );
   }
 
 }
