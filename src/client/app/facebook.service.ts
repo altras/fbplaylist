88e1ea8477bbdc85145236efcbdc49e0849d0f19
Ivo Kacharov <ivor.kach@gmail.com>
2016-11-02 22:58:27 +0200

    initial integration of youtube sdk

--- src/client/app/facebook.service.ts
+++ src/client/app/facebook.service.ts
@@ -24,14 +24,50 @@ export class FacebookService {
   }
 
 
+  youtubeRegex: any = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?/;
+
+
+
+  pushIds(data : any) : void {
+    this.ids.push(
+              data.filter((msg: any) => {
+                // -- YouTube Regular expression that matches patterns such as:
+                // http://www.youtube.com/watch?v=iwGFalTRHDA
+                // http://www.youtube.com/watch?v=iwGFalTRHDA&feature=related
+                // http://youtu.be/iwGFalTRHDA
+                // http://youtu.be/n17B_uFF4cA
+                // http://www.youtube.com/watch?v=t-ZRX8984sc
+                // http://youtu.be/t-ZRX8984sc
+                if (!msg.link) {
+                  return false;
+                }
+                let link: string = msg.link;
+                let youtubeUrl: any = link.match(this.youtubeRegex);
+                return youtubeUrl && youtubeUrl[1];
+              }).map((message: any) => {
+                let link: string = message.link;
+                let ids: string[] = link.match(this.youtubeRegex);
+                return ids[1];
+              })
+            )
+  }
+
+  ids : string[] = [];
+
   getPostLinks(path: string) : Promise<any> {
-    return new Promise<any>(function(resolve: any, reject: any) {
+    return new Promise<any>((resolve: any, reject: any) => {
       FB.api(path, 'GET', {"fields":"link"},
-        function(res: any) {
+        (res: any) => {
           if(res.error) {
             reject(res)
           } else {
-            resolve(res);
+            this.pushIds(res.data)
+            console.log(res)
+            if(res.paging.next) {
+              this.getPostLinks(res.paging.next)
+            } else {
+              resolve(this.ids);
+            }
           }
         }
       );
--- src/client/app/playlist/playlist.component.ts
+++ src/client/app/playlist/playlist.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { FacebookService } from '../facebook.service';
 
 
-
+declare var YT: any;
 
 /**
 * This class represents the lazy loaded PlaylistComponent.
@@ -19,6 +19,7 @@ export class PlaylistComponent implements OnInit {
 
   link: string;
   isFacebookLoggedIn: boolean = true;
+  player : any;
 
   constructor(private router: Router, private route: ActivatedRoute, private facebookService: FacebookService) {
     facebookService.init();
@@ -28,6 +29,22 @@ export class PlaylistComponent implements OnInit {
     let queryParams: any = this.route.queryParams;
     this.link = queryParams.getValue().link
     this.facebookService.getLoginStatus().then(res => this.onLoginStatus(res))
+  }
+
+  onPlayerReady(event:any) : void {
+    event.target.playVideo();
+  }
+
+  done : boolean = false;
+  onPlayerStateChange(event : any) : void {
+        if (event.data == YT.PlayerState.PLAYING && !this.done) {
+          setTimeout(this.stopVideo, 6000);
+          this.done = true;
+        }
+  }
+
+  stopVideo() : void {
+        this.player.stopVideo();
   }
 
   onLoginClick() : void {
@@ -46,7 +63,50 @@ export class PlaylistComponent implements OnInit {
 
   onLinksSuccess(res: any) : void {
     console.log(res)
+    var resultLinks : any;
+
+    let youtubeIds =
+      res.data.filter((msg: any) => {
+        // -- YouTube Regular expression that matches patterns such as:
+        // http://www.youtube.com/watch?v=iwGFalTRHDA
+        // http://www.youtube.com/watch?v=iwGFalTRHDA&feature=related
+        // http://youtu.be/iwGFalTRHDA
+        // http://youtu.be/n17B_uFF4cA
+        // http://www.youtube.com/watch?v=t-ZRX8984sc
+        // http://youtu.be/t-ZRX8984sc
+        if (!msg.link) {
+          return false;
+        }
+        let youtubeRegex: any = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?/;
+        let link: string = msg.link;
+        let youtubeUrl: any = link.match(youtubeRegex);
+        return youtubeUrl;
+      }).map((message: any) => {
+        let link: string = message.link;
+        let ids: string = link.split('=')[1];
+        return ids.split('&')[0];
+      }).join(',')
+
+       this.player = new YT.Player('player', {
+             height: '390',
+             width: '640',
+             playerVars: {playlist: youtubeIds},
+             events: {
+               'onReady': this.onPlayerReady,
+              //  'onStateChange': this.onPlayerStateChange,
+               'onError' : (event : any) => {console.log(event)}
+             }
+           });
+
   }
+
+getYoutubeId(theLink:string):string {
+  var regexp = new RegExp(/https:\/\/(?:www\.)?youtube.*watch\?v=([a-zA-Z0-9\-_]+)/)
+  if (regexp.test(theLink)) {
+    return theLink.match(regexp)[1]
+  }
+  return "WlBiLNN1NhQ";
+}
 
   onLinksError(res: any) : void {
     console.log(res)
