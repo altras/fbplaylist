fc799d1bb9986a0ed55642dd4785b3a2d65b0ed5
Vitaliy Filipov <altras@gmail.com>
2016-12-09 00:53:04 +0200

    Update run-docker script
    Former-commit-id: 9a0e3fe3e30374422598b7306381c40a68235e13

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker load -i docker-image/playlist-app.tar && sudo docker run --name playlistapp-nginx -v /nginx-logs:/var/log/nginx -p 80:80 -d playlistappg
+sudo docker stop $(sudo docker ps -a -q) && sudo docker rm $(sudo docker ps -a -q) && sudo docker rmi playlistapp && sudo docker load -i docker-image/playlist-app.tar  && sudo docker run --name playlistapp-nginx -v /nginx-logs:/var/log/nginx -p 80:80 -d playlistapp
